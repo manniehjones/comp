30a914df6d1f433ea677bd2c5585208556f6b6fc
Immanuel Jones <manniej@ad.unc.edu>
2023-09-17 19:44:54 -0400

    Add files via upload

--- ex01_chardle.py
+++ ex01_chardle.py
@@ -1,11 +1,42 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__= "730569742"
 
-user_name: str = input("Enter a 5-character word: ")
-character: str = input("Enter a single character: ")
-searching: str = print("Searching for", character, "in", user_name)
-if "{character}, is in {user_name}":
-    print (character, "found at index", user_name.find(character))
-print: ("Searching for", character, "in", user_name)
-if "{character}, is not in {user_name}":
-    print: str = print("No instances of", character, "found in", user_name, )
+# labels
+wordz = input("Enter a 5-character word: ")
+characterz = input("Enter a single character: ")
+
+#errors
+if len(wordz) != 5:
+    print("Error: Word must contain 5 characters.")
+    exit()
+else: 
+    if len(characterz) != 1:
+        print("Error: Character must be a single character.")
+        exit()
+    else: 
+        print("Searching for {characterz} in {wordz}")
+
+# indexes
+count = 0
+
+if wordz[0] == characterz:
+    print("{characterz} found at index 0")
+    count += 1
+if wordz[1] == characterz:
+    print("{characterz} found at index 1")
+    count += 1
+if wordz[2] == characterz:
+    print("{characterz} found at index 2")
+    count += 1
+if wordz[3] == characterz:
+    print("{characterz} found at index 3")
+    count += 1
+if wordz[4] == characterz:
+    print("{characterz} found at index 4")
+    count += 1
+
+# instances
+if count == 0:
+    print("No instances of {characterz} found in {wordz}")
+else:
+    print("{count} instance{'s' if count != 1 else''} of {characterz} found in {wordz}")
